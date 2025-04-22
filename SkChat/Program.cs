c4f08b5bad0f622717209fa89dbd7c20498b3e60
Steve Bowman <steven.bowman@dnv.com>
2025-04-22 12:02:15 +0100

    Day 2: wired Azure OpenAI credentials and chat loop

--- SkChat/Program.cs
+++ SkChat/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT") ?? throw new ArgumentNullException("AZURE_OPENAI_DEPLOYMENT environment variable is not set."),
+        Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new ArgumentNullException("AZURE_OPENAI_ENDPOINT environment variable is not set."),
+        Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY") ?? throw new ArgumentNullException("AZURE_OPENAI_KEY environment variable is not set."))
+    .Build();
+
+Console.WriteLine("Kernel initialized: " + Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT"));
+Console.WriteLine("Deployment name: " + Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT"));
+
+while (true)
+{
+    Console.Write("> ");
+    var user = Console.ReadLine();
+    var reply = await kernel.InvokePromptAsync("{{$input}}", new() { { "input", user } });
+    Console.WriteLine($"Bot: {reply}");
+}
